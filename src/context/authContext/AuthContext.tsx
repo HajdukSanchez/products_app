dbff2ef4b88f3b056f72737544f43b361ddf9eb0
hajduksanchez <hajduksanchez.dev@gmail.com>
2022-05-25 12:29:07 -0500

    🚧 [ADD] AuthContext

--- src/context/authContext/AuthContext.tsx
+++ src/context/authContext/AuthContext.tsx
@@ -1,14 +1,39 @@
-import React, { createContext, ReactNode } from 'react';
-import { AuthContextProps } from '../../models/authContext.model';
+import React, { createContext, ReactNode, useReducer } from 'react';
+
+import { authReducer } from './authReducer';
+import { AuthContextProps, AuthState } from '../../models/authContext.model';
 
 export const AuthContext = createContext({} as AuthContextProps);
+
+const authInitialState: AuthState = {
+  status: 'checking',
+  user: null,
+  token: null,
+  errorMessage: '',
+};
 
 interface AuthProviderProps {
   children: ReactNode | ReactNode[];
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const returnValue: AuthContextProps = {} as any;
+  const [state, dispatch] = useReducer(authReducer, authInitialState);
+
+  const signUp = () => {};
+
+  const signIn = () => {};
+
+  const logOut = () => {};
+
+  const removeError = () => {};
+
+  const returnValue: AuthContextProps = {
+    ...state,
+    signUp,
+    signIn,
+    logOut,
+    removeError,
+  };
 
   return <AuthContext.Provider value={returnValue}>{children}</AuthContext.Provider>;
 };
