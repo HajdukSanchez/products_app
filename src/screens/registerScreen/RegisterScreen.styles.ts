fb98c1d1cd82eeb16b9d9b01888bafdeb4d68954
hajduksanchez <hajduksanchez.dev@gmail.com>
2022-05-25 11:43:15 -0500

    🚧 [ADD] Register screen

--- src/screens/registerScreen/RegisterScreen.styles.ts
+++ src/screens/registerScreen/RegisterScreen.styles.ts
@@ -1,3 +1,20 @@
 import { StyleSheet } from 'react-native';
+import { globalStyles } from '../../theme/appTheme';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  keyBoardView: {
+    flex: 1,
+    backgroundColor: '#5856D6',
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  title: {
+    ...globalStyles.title,
+  },
+  floatingButton: {
+    position: 'absolute',
+  },
+});
--- src/screens/registerScreen/RegisterScreen.tsx
+++ src/screens/registerScreen/RegisterScreen.tsx
@@ -1,11 +1,73 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, KeyboardAvoidingView, Platform, Keyboard } from 'react-native';
 
-const RegisterScreen = () => {
+import { StackScreenProps } from '@react-navigation/stack';
+
+import { styles } from './RegisterScreen.styles';
+import { RootStackParamList } from '../../routes/routes';
+import { Button, Logo, TextInputForm } from '../../components';
+import { useForm } from '../../hooks';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+interface RegisterScreenProps extends StackScreenProps<RootStackParamList, 'Register'> {}
+
+const RegisterScreen = ({ navigation: { replace } }: RegisterScreenProps) => {
+  const {
+    form: { name, email, password },
+    onChange,
+  } = useForm({ name: '', email: '', password: '' });
+  const { top, right } = useSafeAreaInsets();
+
+  const handleRegister = () => {
+    Keyboard.dismiss(); // Dismiss keyboard
+    // TODO: Add Register logic
+  };
+
+  const handleLogin = () => {
+    replace('Login');
+  };
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <>
+      <KeyboardAvoidingView style={styles.keyBoardView} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <View style={styles.container}>
+          <Logo />
+          <Text style={styles.title}>Register</Text>
+          <TextInputForm
+            placeholder="Name"
+            placeholderTextColor={'rgba(255,255,255,0.4)'}
+            keyboardType={'default'}
+            autoCapitalize={'words'}
+            autoCorrect={false}
+            onChangeText={text => onChange(text, 'name')}
+            value={name}
+          />
+          <TextInputForm
+            placeholder="Email"
+            placeholderTextColor={'rgba(255,255,255,0.4)'}
+            keyboardType={'email-address'}
+            autoCapitalize={'none'}
+            autoCorrect={false}
+            onChangeText={text => onChange(text, 'email')}
+            value={email}
+          />
+          <TextInputForm
+            placeholder="Password"
+            placeholderTextColor={'rgba(255,255,255,0.4)'}
+            keyboardType={'default'}
+            autoCapitalize={'none'}
+            autoCorrect={false}
+            onChangeText={text => onChange(text, 'password')}
+            value={password}
+            isPassword
+          />
+          <Button text="Register" onPress={handleRegister} />
+          <View style={{ ...styles.floatingButton, top: top + 10, right: right - 20 }}>
+            <Button text="Login" withBorder={false} onPress={handleLogin} />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
